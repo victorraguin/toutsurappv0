7d900e125c4dd4e0ad8d3a41785ade49ecccc59b
victorraguin <victor.raguin@live.fr>
2021-07-21 09:51:42 +0200

    Routes updated and modification of the categories design page

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 // == Import : npm
 import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
 import { render } from 'react-dom';
 import 'semantic-ui-css/semantic.min.css';
 
@@ -10,7 +11,7 @@ import ToutSurApp from 'src/components/ToutSurApp';
 // == Render
 // 1. Élément React racine (celui qui contient l'ensemble de l'app)
 //    => crée une structure d'objets imbriqués (DOM virtuel)
-const rootReactElement = <ToutSurApp />;
+const rootReactElement = <Router><ToutSurApp /></Router>;
 // 2. La cible du DOM (là où la structure doit prendre vie dans le DOM)
 const target = document.getElementById('root');
 // 3. Déclenchement du rendu de React (virtuel) => DOM (page web)
