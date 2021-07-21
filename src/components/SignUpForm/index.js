ec08818c3b0d5b33d49db8e2d897a24e18bf0b4b
RoryKillick <rorykillick@gmail.com>
2021-07-21 13:58:26 +0200

    commit

---
+++ src/components/SignUpForm/index.js
@@ -0,0 +1,18 @@
+// == Import npm
+import e from 'express';
+import React from 'react';
+import { Form, Divider, Button } from 'semantic-ui-react';
+
+
+// == Import
+import './styles.scss';
+
+// == Composant
+const SignUpForm = ({form}) => (
+  <div className='signup-div'>
+    
+  </div>
+);
+
+// == Export
+export default SignUpForm;
