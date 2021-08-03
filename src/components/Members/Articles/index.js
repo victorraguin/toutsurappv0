f2bf2963f3d34424568ee23f38bc53468972ad06
RoryKillick <rorykillick@gmail.com>
2021-08-03 23:24:50 +0200

    gutters updated

--- src/components/Members/Articles/index.js
+++ src/components/Members/Articles/index.js
@@ -1,6 +1,6 @@
 // == Import npm
 import React from 'react';
-import { Card, Segment, Container, Loader, Icon } from 'semantic-ui-react';
+import { Card, Segment, Container, Loader, Icon, Grid } from 'semantic-ui-react';
 import { Link } from 'react-router-dom';
 import Article from './Article';
 
@@ -21,13 +21,17 @@ const ArticlesMember = ({ articles, setUserBookmarksArticles, isLoading, visible
     <h3>Bonne lecture sur toutSur.app !</h3>
     </Segment>
       <Card.Group className="card-group" centered>
+        <Grid columns={5} doubling relaxed>
         { articles ? 
         isLoading ? <Loader active size='big' inline='centered'/> 
         :
           articles.map((article) => (
-            <Article article={article} setUserBookmarksArticles={setUserBookmarksArticles} />
+            <Grid.Column>
+              <Article article={article} setUserBookmarksArticles={setUserBookmarksArticles} />
+            </Grid.Column>
           )) : <h4>Vous n'avez pas encore enregistré de catégories dans vos favoris ! N'oubliez pas de personnaliser votre feed</h4>
-    }
+        }
+        </Grid>
       </Card.Group>
       <Icon
       className='scroll-up-button'
