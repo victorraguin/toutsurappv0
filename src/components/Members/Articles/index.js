25812d3fcb1f1e5000bfc601267317dccdfe1ba5
RoryKillick <rorykillick@gmail.com>
2021-08-04 10:07:07 +0200

    centered cards on homepage + h4

--- src/components/Members/Articles/index.js
+++ src/components/Members/Articles/index.js
@@ -20,18 +20,16 @@ const ArticlesMember = ({ articles, setUserBookmarksArticles, isLoading, visible
       <Segment vertical >
     <h3>Bonne lecture sur toutSur.app !</h3>
     </Segment>
-      <Card.Group className="card-group" centered>
-        <Grid columns={5} doubling relaxed>
+      <Card.Group className="card-group">
+        {/* <Grid columns={5} doubling relaxed> */}
         { articles ? 
         isLoading ? <Loader active size='big' inline='centered'/> 
         :
           articles.map((article) => (
-            <Grid.Column>
               <Article article={article} setUserBookmarksArticles={setUserBookmarksArticles} />
-            </Grid.Column>
-          )) : <h4>Vous n'avez pas encore enregistré de catégories dans vos favoris ! N'oubliez pas de personnaliser votre feed</h4>
+          )) : <h4 className="h4center">Vous n'avez pas encore enregistré de catégories dans vos favoris ! N'oubliez pas de personnaliser votre feed</h4>
         }
-        </Grid>
+        {/* </Grid> */}
       </Card.Group>
       <Icon
       className='scroll-up-button'
